1c3f89a3b21af823ff1cef1ed6902e11d62bf53f
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-11-19 14:56:38 +0200

    implemented models and admin site for api

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Report, ReportItem, Meter
+
+
+class ReportItemInline(admin.StackedInline):
+    model = ReportItem
+
+
+class MeterAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'meter_number',
+        'price_per_unit',
+        'unit_name',
+        'total_consumed',
+    )
+    list_filter = (
+        'updated_at'
+    )
+    search_fields = (
+        'name',
+    )
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_filter = (
+        'is_paid',
+    )
+    inlines = [ReportItemInline]
+
+
+admin.site.register(Meter, MeterAdmin)
+admin.site.register(Report, ReportAdmin)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,36 +1,113 @@
 import uuid
+from decimal import Decimal
 
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
-class Bill(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=6, decimal_places=2)
-    paid = models.BooleanField(default=False, blank=True, null=True)
-
-    def __str__(self) -> str:
-        if self.paid:
-            return f'Bill for the {self.created_at.month}, total: {self.total}, paid.'
-        return f'Bill for the {self.created_at.month}, total: {self.total}, not paid.'
-
-
-class Counter(models.Model):
-    id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
-    name = models.CharField(max_length=75)
-    number = models.CharField(max_length=100)
+class Meter(models.Model):
+    id = models.UUIDField(
+        editable=False,
+        primary_key=True,
+        default=uuid.uuid4
+    )
+    name = models.CharField(max_length=50, unique=True)
+    meter_number = models.CharField(
+        max_length=50,
+        unique=True,
+        null=True,
+        blank=True
+        )
+    price_per_unit = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        blank=True,
+        default=Decimal(0.00),
+        validators=[MinValueValidator(0)]
+    )
+    unit_name = models.CharField(max_length=30)
+    total_consumed = models.IntegerField(
+        default=0,
+        validators=[MinValueValidator(0)]
+        )
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        verbose_name = 'Meter'
+        verbose_name_plural = 'Meters'
+        ordering = ['name']
 
-class CounterRecord(models.Model):
-    created_at = models.DateField(auto_now_add=True)
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
-    counter = models.ForeignKey(
-        Counter,
-        on_delete=models.CASCADE,
-        related_name='records'
+
+class Report(models.Model):
+    id = models.UUIDField(
+        editable=False,
+        primary_key=True,
+        default=uuid.uuid4
         )
+    created_at = models.DateTimeField(
+        editable=False,
+        auto_now_add=True
+        )
+    bill = models.FileField(
+        null=True,
+        blank=True,
+        upload_to='bills/%Y/%m/'
+    )
+
+    @property
+    def is_paid(self):
+        if self.bill is None:
+            return False
+        return True
+
+    @property
+    def total(self):
+        items = self.items.all()
+        return sum([item.total for item in items])
 
     def __str__(self) -> str:
-        return f'{self.counter}: {self.amount} for the {self.created_at.month}'
+        month = self.created_at.month
+        year = self.created_at.year
+        return f'Report for the {month} of {year}'
+
+    class Meta:
+        verbose_name = 'Report'
+        verbose_name_plural = 'Reports'
+        ordering = ['-created_at']
+
+
+class ReportItem(models.Model):
+    id = models.UUIDField(
+        editable=False,
+        primary_key=True,
+        default=uuid.uuid4
+    )
+    meter = models.ForeignKey(
+        Meter,
+        on_delete=models.DO_NOTHING,
+        related_name='items'
+        )
+    report = models.ForeignKey(
+        Report,
+        on_delete=models.CASCADE,
+        related_name='items'
+        )
+    consume = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    created_at = models.DateField(auto_now_add=True, editable=False)
+
+    @property
+    def total(self):
+        return round(self.consume * self.meter.price_per_unit, 2)
+
+    def __str__(self) -> str:
+        if self.report.is_paid:
+            return f'You paid {self.total} for {self.meter.name}.'
+        return f'You need to pay {self.total} for {self.meter.name}.'
+
+    class Meta:
+        verbose_name = 'Report Item'
+        verbose_name_plural = 'Report Items'
+        ordering = ['-created_at', 'total']
