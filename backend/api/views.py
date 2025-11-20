2d0e2896e7e290130bfe3460f40f4a81360d495d
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-11-20 13:32:44 +0200

    add viewsets

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,1 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from api.models import Meter, Report
+from api.serializers import MeterSerializer, ReportSerializer
+
+
+class MeterViewSet(ModelViewSet):
+    queryset = Meter.objects.all()
+    serializer_class = MeterSerializer
+
+
+class ReportViewSet(ModelViewSet):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
