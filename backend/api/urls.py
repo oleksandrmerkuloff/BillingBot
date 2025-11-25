1e74df9716c8f47dce5c5261604487e3574bae76
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-11-25 17:30:44 +0200

    add router

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
+from rest_framework import routers
+
+from api.views import MeterViewSet, ReportViewSet
 
 
-urlpatterns = []
+api_router = routers.SimpleRouter()
+api_router.register('meters', MeterViewSet)
+api_router.register('reports', ReportViewSet)
+
+
+urlpatterns = api_router.urls
